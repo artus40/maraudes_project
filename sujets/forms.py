0ca1d0bf8fee1e673c80a935b4232c54ffa08562
Arthur Gerbaud <artus40@users.noreply.github.com>
2016-11-15 15:21:54 +0100

    issue #11

--- sujets/forms.py
+++ sujets/forms.py
@@ -2,10 +2,17 @@ from django.forms import ModelForm
 from django.forms.extras.widgets import SelectDateWidget
 from .models import Sujet
 
+import datetime
+
+current_year = datetime.date.today().year
+YEAR_CHOICE = (year - 2 for year in range(current_year, current_year + 10))
+
 class SujetCreateForm(ModelForm):
     class Meta:
         model = Sujet
         fields = ['nom', 'surnom', 'prenom', 'genre', 'premiere_rencontre']
         widgets = {
-            'premiere_rencontre': SelectDateWidget(empty_label=("Année", "Mois", "Jour")),
+            'premiere_rencontre': SelectDateWidget( empty_label=("Année", "Mois", "Jour"),
+                                                    years = YEAR_CHOICE,
+                                                    ),
         }
