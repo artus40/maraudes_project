2fd7dd9506d59f30ccc4a5430687cfedae0c4e2e
artus40 <lecornemuseux@gmail.com>
2017-01-30 19:59:58 +0100

    essayé de généraliser le code pour les modaux bootstrap, non testé git status

---
+++ website/static/scripts/bootstrap-modal.js
@@ -0,0 +1,41 @@
+<script type="text/javascript">
+    /* Lier les boutons de création
+    *  Thanks to Derek Morgan, https://dmorgan.info/posts/django-views-bootstrap-modals/
+    */
+  $(function() {
+
+    var formAjaxSubmit = function(form, modal) {
+     	$(form).submit(function (e) {
+       	 	e.preventDefault();
+         	$.ajax({
+             		type: $(this).attr('method'),
+             		url: $(this).attr('action'),
+             		data: $(this).serialize(),
+             		success: function (xhr, ajaxOptions, thrownError) {
+                 		if ( $(xhr).find('.has-error').length > 0 || $(xhr).find('.alert-danger').length > 0) {
+                     			$(modal).find('.modal-body').html(xhr);
+                     			formAjaxSubmit(form, modal);
+                 		} else {
+                     			$(modal).modal('toggle');
+                    			// Reload page ?
+                     			location.reload(true)
+                 		}
+             		},
+             		error: function (xhr, ajaxOptions, thrownError) {
+                 		// handle response errors here
+             			}
+         		});
+      		});
+	};
+
+    var setLinkEvent = function(id, href, title) {
+	$('#'+id).click(function() {
+        	$('#form-modal-body').load(href, function() { // + '?next={% url "maraudes:create" pk=maraude.id %}' ??
+        		$('.modal-title').text(title);
+            		$('#form-modal').modal('toggle');
+            		formAjaxSubmit("#form-modal-body form", "#form-modal");
+        		});
+    		});
+   	 };
+  });
+</script>
